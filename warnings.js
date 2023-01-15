38c1a4d11b7eb6ecbf56c9bbebff560cbdd0a175
Programozás Karrier <info@programozaskarrier.hu>
2023-01-15 21:04:02 +0100

    done

---
+++ warnings.js
@@ -0,0 +1,15 @@
+
+export const showNotification = (msg)=>{
+
+    const notificationElement = document.querySelector(".notification");
+    
+    notificationElement.innerHTML = msg;
+    
+    notificationElement.classList.add('notif-enter')
+    
+    setTimeout(()=>{
+        notificationElement.classList.remove('notif-enter');
+    }, 2000)
+    
+    };
+    
